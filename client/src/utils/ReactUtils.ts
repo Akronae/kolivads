161dbed9780542eeb002b1dc438940d01233d092
Alexandre Daubricourt <alexandre@daubricourt.com>
2022-07-11 14:02:38 +0200

    handling SVGs

--- client/src/utils/ReactUtils.ts
+++ client/src/utils/ReactUtils.ts
@@ -9,6 +9,7 @@ export function getNodeText(node: ReactNode): string {
   if (!node) return '';
   if (['string', 'number'].includes(typeof node)) return node.toString();
   if (node instanceof Array) return node.map(getNodeText).join('');
-  if (typeof node === 'object' && node) return getNodeText(node.props.children);
+  if (typeof node === 'object' && node)
+    return getNodeText(node['props'].children);
   return node.toString();
 }
